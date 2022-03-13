e79bce7c852eaebbe119e9d4ec1fc854394d13ae
SomeRandomGuy64 <faizan163123@gmail.com>
2022-03-13 13:31:15 +0000

    setting up JavaFX

--- src/CHIP8.java
+++ src/CHIP8.java
@@ -1,6 +1,9 @@
 import java.util.Date;
+import javafx.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class CHIP8 {
+public class CHIP8 extends Application {
 
     Renderer renderer;
     private int fps;
@@ -12,14 +15,15 @@ public class CHIP8 {
         fpsInterval = 1000/fps;
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        // TODO Auto-generated method stub
+        stage.setTitle("CHIP-8 Emulator");
+        Pane root = new Pane();
+    }
+
     public static void main(String[] args) 
     {
-
-    }
-
-    public void step()
-    {
-
-    }
-    
+        launch(args);
+    }  
 }
