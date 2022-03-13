edcc360cc7c9b5e98bf79e0f89164984f3453926
SomeRandomGuy64 <faizan163123@gmail.com>
2022-03-13 12:45:18 +0000

    starting renderer

---
+++ Renderer.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+public class Renderer 
+{   
+    //creates ints for columns, rows and scale
+    private int columns, rows, scale;
+
+    //contructor
+    public Renderer()
+    {   
+        //64 columns on the CHIP-8 display
+        columns = 64;
+        //32 rows on the CHIP-8 display
+        rows = 32;
+        //as a resolution of 64x32 is incredibly small I'll be using scale to multiply the resolution
+        //set scale for now but will change to a user input later
+        scale = 10;
+    }
+
+    public int getColumns()
+    {
+        return columns;
+    }
+
+    public int getRows()
+    {
+        return rows;
+    }
+
+    public int getScale()
+    {
+        return scale;
+    }
+}
