d35d53b8ad6cf604c13dd9247c5395289ec56586
SomeRandomGuy64 <faizan163123@gmail.com>
2022-03-13 13:05:58 +0000

    Started on the chip8 class

--- CHIP8.java
+++ CHIP8.java
@@ -1,3 +1,25 @@
+import java.util.Date;
+
 public class CHIP8 {
+
+    Renderer renderer;
+    private int fps;
+    private double fpsInterval;
+    private Date startTime, now, then, elapsed;
+
+    public CHIP8()
+    {
+        fpsInterval = 1000/fps;
+    }
+
+    public static void main(String[] args) 
+    {
+        
+    }
+
+    public void step()
+    {
+
+    }
     
 }
