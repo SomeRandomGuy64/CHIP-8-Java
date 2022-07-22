7a2ad8fb7a83e4c429710347daa9c342ad09b71e
SomeRandomGuy64 <faizan163123@gmail.com>
2022-07-22 21:55:56 +0100

    CPU Stuff

--- src/CPU.java
+++ src/CPU.java
@@ -1,3 +1,26 @@
-public class CPU {
-    
+import java.util.Stack;
+import java.util.ArrayList;
+
+public class CPU 
+{
+    private int memoryAddresses, delayTimer, soundTimer;
+    private Stack<Integer> stack = new Stack<>();
+    private boolean paused;
+
+    public CPU(Renderer renderer)
+    {
+        memoryAddresses = 0;
+
+        delayTimer = 0;
+        soundTimer = 0;
+
+
+    }
+
+    public void loadSpritesIntoMemory() 
+    {
+        ArrayList<Integer> sprites = new ArrayList<>();
+        
+    }
+
 }
