0c2813bcb281498901fdd7bc75427b3b2c226537
SomeRandomGuy64 <faizan163123@gmail.com>
2022-07-23 13:13:21 +0100

    stuff

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.*;
@@ -6,13 +8,31 @@ import javafx.scene.layout.Pane;
 public class Main extends Application {
 
     Renderer renderer;
+    CPU cpu;
+
+    private long fps = 60, fpsInterval, startTime, now, then, elapsed, loop;
 
     @Override
     public void start(Stage stage) throws Exception {
 
+        fpsInterval = 1000 / fps;
+        then = System.currentTimeMillis();
+        startTime = then;
+
+        //cpu.loadSpritesIntoMemory();
+        //cpu.loadROM('BLINKY');
+        //javascript code
+        //loop = requestAnimationFrame(step);
+
+
         stage.setTitle("CHIP-8 Emulator");
         Pane root = new Pane();
         renderer = new Renderer(root);
+
+        //testing purposes, remove when not testing
+        renderer.testRender();
+        renderer.render();
+
         stage.setScene(new Scene(root, renderer.getScaledColumns(), renderer.getScaledRows()));
         stage.show();
     }
@@ -21,4 +41,18 @@ public class Main extends Application {
     {
         launch(args);
     }  
+
+    public void step()
+    {
+        now = System.currentTimeMillis();
+        elapsed = now - then;
+
+        if (elapsed > fpsInterval)
+        {
+            cpu.cycle();
+        }
+
+        //javascript code
+        //loop = requestAnimationFrame(step);
+    }
 }
